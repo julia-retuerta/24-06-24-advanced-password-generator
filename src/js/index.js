03eb73474cedc6798f049945e8d605a8b86b145b
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-06-24 23:27:06 +0200

    practica-casa-24-junio

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
+// EXPLICACIONES
+
+// const generatePassword -> función que se encarga de generar el bucle
+//  const randomPosition y  const randomCharacter -> función que se encarga de generar el carácter aleatorio
+// finalPassword = ''; -> sirve para que, cada vez que se pulsa el botón, se vacíe el campo de la ccontraseña y se genere una nueva.
+// passwordElement.textContent = finalPassword -> función que se encarga de escribir el final password
+
 // PASOS
 
 //Sincronizar el length del nuevo password con el input range
@@ -9,6 +16,10 @@ import '../scss/styles.scss';
 //Que al darle al botón de generar contraseña, se genere una contrseña de la longitud que le hemos dicho
 
 const availableCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
+const upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+const numbersCharacters = '0123456789';
+const symbolsCharacters = '!@#$%^&*()_+-={}[]:;<>,.?/';
 
 const passwordElement = document.getElementById('password');
 const rangeLabelElement = document.getElementById('range-label');
@@ -25,20 +36,38 @@ const updateLabel = () => {
 
 rangeInputElement.addEventListener('input', updateLabel);
 
-// función que se encarga de generar el bucle
+const optionsChecked = () => {
+  const inputElements = document.querySelectorAll('input[type="checkbox"]:checked');
+};
+
+generatePasswordButtonElement.addEventListener('click', optionsChecked);
+
 const generatePassword = () => {
   finalPassword = '';
   for (let i = 0; i < passwordLength; i++) {
-    // función que se encarga de generar el carácter aleatorio
     const randomPosition = Math.floor(Math.random() * availableCharacters.length);
     const randomCharacter = availableCharacters.charAt(randomPosition);
     finalPassword += randomCharacter;
   }
 
-  // función que se encarga de escribir el final password
   passwordElement.textContent = finalPassword;
 };
 
 generatePasswordButtonElement.addEventListener('click', generatePassword);
 
-//finalPassword = ''; esto sirve para que cada vez que se pulsa el botón, se vacíe el campo de la ccontraseña y se genere una nueva.
+/* const checkboxButtonElement = document.getElementById('checkbox-button');
+const checkboxInfoElement = document.getElementById('checkbox-info');
+
+const checkboxChecked = () => {
+  // esta constante hay que declararla dentro de la función para que funcione cuando se presiona el botón
+  const inputsElements = document.querySelectorAll('input[type="checkbox"]:checked');
+  let resultText = `Hay ${inputsElements.length} checkbox marcados: `;
+
+  inputsElements.forEach(input => {
+    resultText += input.value + ' ';
+  });
+
+  checkboxInfoElement.textContent = resultText;
+};
+
+checkboxButtonElement.addEventListener('click', checkboxChecked); */
